0367d23be57c5391788609b6dc6f3a6cb8734c7e
Cezar Sas <cezar.sas@gmail.com>
2023-06-04 17:59:49 +0200

    Setting up grammars

--- src/parser/java.py
+++ src/parser/java.py
@@ -1,5 +1,3 @@
-from tree_sitter import Language
-
 from entity.project import File
 from parser.parser import AbstractParser
 
@@ -10,10 +8,7 @@ class ParserJava(AbstractParser):
     """
     def __init__(self):
         super().__init__()
-        self.LANGUAGE = Language('/home/sasce/PycharmProjects/AutoFL/src/resources/languages.so', 'python')
-        self.parser.set_language(self.LANGUAGE)
+
 
     def parse(self, file: File):
-        code_b = bytes(file.content, "utf8")
-        tree = self.parser.parse(code_b)
-        return self._traverse(code_b, tree)
+        pass
